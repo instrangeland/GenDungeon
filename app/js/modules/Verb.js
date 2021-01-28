65f6c0b1874fd002f14b79baaa716ac04da74468
Julian <julian@lachniet.com>
2021-01-27 20:28:09 -0500

    Revert "Allow inputs to contains extra ending words"
    This reverts commit b790438c

--- app/js/modules/Verb.js
+++ app/js/modules/Verb.js
@@ -17,23 +17,21 @@ export default class Verb {
         const matchArray = match.split(' ');
         const args = [];
 
-        if (matchArray.length > test.length) {
+        if (matchArray.length !== test.length) {
             return {
                 matched: false
             };
         }
 
         for (const [index, testWord] of test.entries()) {
-            if (index < matchArray.length) {
-                const matchWords = matchArray[index].split(',');
-                if (!matchWords.includes(testWord) && !matchWords.includes('#')) {
-                    return {
-                        matched: false
-                    };
-                }
-                if (matchWords.includes('#')) {
-                    args.push(testWord);
-                }
+            const matchWords = matchArray[index].split(',');
+            if (!matchWords.includes(testWord) && !matchWords.includes('#')) {
+                return {
+                    matched: false
+                };
+            }
+            if (matchWords.includes('#')) {
+                args.push(testWord);
             }
         }
 
